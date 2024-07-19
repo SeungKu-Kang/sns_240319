3315f1a110b1be0133a7837b9644327ff515ffc3
KANG\ktc user <yoyuman@gmail.com>
2024-07-19 14:15:56 +0900

    0719 집

--- src/main/java/com/sns/post/PostRestController.java
+++ src/main/java/com/sns/post/PostRestController.java
@@ -4,7 +4,11 @@ import java.util.Map;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import jakarta.servlet.http.HttpSession;
 
 @RequestMapping("/post")
 @RestController
@@ -12,7 +16,17 @@ public class PostRestController {
 
 //	@PostMapping("/create")
 //	public Map<String, Object> create(
-//			) {
+//			@RequestParam("content") String content,
+//			@RequestParam(value = "file", required = false) MultipartFile file,
+//			HttpSession session) {
+//		
+//		// 글쓴이 번호를 session에서 꺼낸다.
+//		int userId = (int)session.getAttribute("userId");
+//		String userLoginId = (String)session.getAttribute("userLoginId");
+//		
+//		// DB insert
+//		postBO.addPost()
+//		// 응답값
 //		
 //	}
 }
